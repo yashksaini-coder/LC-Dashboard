d2bdc425037c2540a3852e64572fd8edb5f23c7f
yashksaini-coder <ys3853428@gmail.com>
2025-03-04 22:34:03 +0530

    feat: ✨ Enhance layout structure with viewport meta tag and main container

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-
 export const metadata: Metadata = {
   title: "LC Dashboard",
   description: "NEXT.JS application to fetch and display LeetCode problems",
@@ -10,8 +9,13 @@ export const metadata: Metadata = {
 export default function RootLayout({ children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body className="min-h-screen w-full">
+        <main className="container mx-auto px-2 sm:px-4 md:px-6 py-4">
+          {children}
+        </main>
       </body>
     </html>
   );
